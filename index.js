508c07832f5eb0beaafb021a617880ecf04f47ea
Vaishnavi Reddy <vaireddy11@gmail.com>
2016-06-13 10:20:59 -0700

    separate routes

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var engines = require('consolidate');
-
-var verifyUser = require('./helpers').verifyUser;
-var getUserfromID = require('./helpers').getUserfromID;
-var getUserfromEmail = require('./helpers').getUserfromEmail;
-var getFeed = require('./helpers').getFeed;
-var addFeedItem = require('./helpers').addFeedItem;
+var routes = require('./routes');
 
 var currentUser;
 
@@ -24,49 +19,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
-app.get('/', function (req, res) {
-  res.render('login', {});
-});
-
-app.post('/authenticate', function(req, res) {
-  console.log("/auth", req.body);
-  var email = req.body.email;
-  var userPromise = verifyUser(email, req.body.password);
-  userPromise.then(function(row) {
-    getUserfromEmail(email).then(function(row) {
-      currentUser = row.id;
-    })
-    res.redirect('/feed');
-  }).catch(function(error) {
-    res.redirect('/');
-  });
-});
-
-app.get('/feed', function(req, res) { 
-  var feedPromises = getFeed();
-  feedPromises.then(function(result) {
-      res.render('feed', {feed: result.reverse(), user: currentUser});
-  })
-})
-
-app.post('/feed', function(req, res) {
-  console.log('req.body.feed_message', req.body.feed_message);
-  addFeedItem(req.body.feed_message, currentUser).then(function(result) {
-    console.log("data", result);
-  }).catch(function(err) {
-    console.log("err", err);
-  });
-  res.redirect('/feed');
-});
-
-app.get('/:user', function(req, res) {
-  var user = req.params.user;
-  res.render('profile', {user: user});
-});
-
-app.post('/logout', function(req, res) {
-  res.redirect('/login');
-})
+app.use('/', routes);
 
 app.listen(3000, function() {
    console.log("listening on 3000");
---
+++ routes.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var router = express.Router();
+
+var verifyUser = require('./helpers').verifyUser;
+var getUserfromID = require('./helpers').getUserfromID;
+var getUserfromEmail = require('./helpers').getUserfromEmail;
+var getFeed = require('./helpers').getFeed;
+var addFeedItem = require('./helpers').addFeedItem;
+
+var currentUser;
+
+router.get('/', function (req, res) {
+  res.render('login', {});
+});
+
+router.post('/authenticate', function(req, res) {
+  console.log("/auth", req.body);
+  var email = req.body.email;
+  var userPromise = verifyUser(email, req.body.password);
+  userPromise.then(function(row) {
+    getUserfromEmail(email).then(function(row) {
+      currentUser = row.id;
+    })
+    res.redirect('/feed');
+  }).catch(function(error) {
+    res.redirect('/');
+  });
+});
+
+router.get('/feed', function(req, res) { 
+  var feedPromises = getFeed();
+  feedPromises.then(function(result) {
+      res.render('feed', {feed: result.reverse(), user: currentUser});
+  })
+})
+
+router.post('/feed', function(req, res) {
+  console.log('req.body.feed_message', req.body.feed_message);
+  addFeedItem(req.body.feed_message, currentUser).then(function(result) {
+    console.log("data", result);
+  }).catch(function(err) {
+    console.log("err", err);
+  });
+  res.redirect('/feed');
+});
+
+router.get('/:user', function(req, res) {
+  var user = req.params.user;
+  res.render('profile', {user: user});
+});
+
+router.post('/logout', function(req, res) {
+  currentUser = null;
+  res.redirect('/login');
+})
+
+module.exports = router;
